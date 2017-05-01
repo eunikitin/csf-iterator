6335ebb2db06e7eed9211b3ae92d0d714044b3f6
eunikitin <eu.nikitin@gmail.com>
2017-05-01 22:37:26 +0300

    Setup test tools

---
+++ webpack.config.test.babel.js
@@ -0,0 +1,11 @@
+import merge        from 'webpack-merge';
+
+import baseConfig   from './webpack.config.base';
+import resolveRules from './builder/resolve';
+import es6Loader    from './builder/loaders/es6';
+
+
+export default merge(
+    baseConfig,
+    resolveRules
+);

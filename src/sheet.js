33e2359f3050de23f0c2bbe05ca666ff74d2610f
eunikitin <eu.nikitin@gmail.com>
2017-05-11 09:14:07 +0300

    Add constructor to Sheet class

--- src/sheet.js
+++ src/sheet.js
@@ -1,2 +1,8 @@
 
-export default class Sheet {}
+export default class Sheet {
+  constructor(data, name = null) {
+    if (!data) throw Error('data argument is required');
+    this.data = data;
+    this.name = name;
+  }
+}
---
+++ test/specs/sheet.spec.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+
+import Sheet from 'Src/sheet';
+
+import sheet1x1 from 'Test/fixtures/sheet-1x1';
+
+
+describe('Sheet', () => {
+  let sheet;
+  
+  it('should have data property', () => {
+    sheet = new Sheet(sheet1x1);
+    expect(sheet).to.have.property('data');
+  });
+
+  it('should have name property', () => {
+    sheet = new Sheet(sheet1x1);
+    expect(sheet).to.have.property('name');
+  });
+
+  it('should set data property on initialization', () => {
+    sheet = new Sheet(sheet1x1);
+    expect(sheet.data).to.equal(sheet1x1);
+  });
+
+  it('should throw an Error if data property was not given to constructor', () => {
+    expect(() => new Sheet()).to.throw(Error);
+  });
+});

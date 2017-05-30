1074c992945fee9f89e1c0e4108b8cec31635402
eunikitin <eu.nikitin@gmail.com>
2017-05-30 22:36:00 +0300

    Add 5x2 fixture

---
+++ test/fixtures/5x2/index.js
@@ -0,0 +1,7 @@
+import data from './data';
+import object from './sheet';
+
+export default {
+  data,
+  object,
+};
---
+++ test/fixtures/5x2/sheet.js
@@ -0,0 +1,12 @@
+import data from './data';
+
+
+export default {
+  data,
+  width: 5,
+  height: 2,
+  firstColumn: 1,
+  firstRow: 1,
+  lastColumn: 5,
+  lastRow: 2,
+};

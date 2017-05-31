2310d56f81ac7294c94fa0202a22ae55b3d1eae9
eunikitin <eu.nikitin@gmail.com>
2017-05-31 14:30:46 +0300

    Add array of arrays data fixtures

---
+++ test/fixtures/2x2-offset/data-aoa.js
@@ -0,0 +1,6 @@
+import data from './data';
+
+export default [
+  [data.C3, data.D3],
+  [data.C4, data.D4],
+];
---
+++ test/fixtures/2x2/data-aoa.js
@@ -0,0 +1,6 @@
+import data from './data';
+
+export default [
+  [data.A1, data.B1],
+  [data.A2, data.B2],
+];

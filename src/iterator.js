7262bfa4ed34447ccf6ad6a9a462af084ecb59e3
eunikitin <eu.nikitin@gmail.com>
2017-05-31 20:19:19 +0300

    Add Iterator.getRow method

--- src/iterator.js
+++ src/iterator.js
@@ -1,3 +1,5 @@
+import utils from 'csf-utils';
+
 
 export default class Iterator {
   constructor(sheet) {
@@ -8,5 +10,29 @@ export default class Iterator {
   static selectColumns(columns) {
     return columns.sort((a, b) => a - b)
       .filter((value, index, self) => self.indexOf(value) === index);
+  }
+
+  getRow(index, columns, move = true) {
+    const row = (index === undefined || index === null) ? this.position : index;
+
+    if (this.sheet.firstRow > row || row > this.sheet.lastRow) return null;
+
+    const self = this;
+
+    const columnList = columns ? Iterator.selectColumns(columns)
+      : Iterator.selectColumns(
+        Array(null, ...{ length: self.sheet.width - 1 }) // eslint-disable-line no-array-constructor
+          .map((arg1, columnIndex) => self.sheet.lastColumn - columnIndex, Number),
+      );
+
+    const record = [];
+
+    columnList.forEach((column) => {
+      record.push(this.sheet.data[utils.columnName.intToExcelCol(column) + row]);
+    });
+
+    if (move) this.position += 1;
+
+    return record;
   }
 }

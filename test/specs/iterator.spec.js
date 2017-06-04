aa84139582a3afe60de1e54221567632c6ac0a48
eunikitin <eu.nikitin@gmail.com>
2017-06-04 19:43:15 +0300

    Move 5x2 test suite in iterator.spec.js

--- test/specs/iterator.spec.js
+++ test/specs/iterator.spec.js
@@ -159,6 +159,68 @@ describe('Iterator', () => {
       });
     });
 
+    describe('5x2', () => {
+      beforeEach(() => {
+        iterator = new Iterator(sheet5x2Object);
+        position = iterator.position;
+      });
+
+      it('should return row with index of current iterator position if index argument does not specified', () => {
+        const record = iterator.getRow();
+        expect(record).to.deep.equal(sheet5x2DataAoa[position]);
+      });
+
+      it('should return row with index of current iterator position if index argument equals null', () => {
+        const record = iterator.getRow(null);
+        expect(record).to.deep.equal(sheet5x2DataAoa[position]);
+      });
+
+      it('should return whole row if columns argument does not specified', () => {
+        const record = iterator.getRow(position);
+        expect(record).to.deep.equal(sheet5x2DataAoa[position]);
+      });
+
+      it('should return whole row if columns argument equals null', () => {
+        const record = iterator.getRow(position, null);
+        expect(record).to.deep.equal(sheet5x2DataAoa[position]);
+      });
+
+      it('should return row with specified only columns', () => {
+        const columns = [1, 2, 4];
+        const record = iterator.getRow(position, columns);
+        expect(record).to.deep.equal([
+          sheet5x2DataAoa[position][columns[0] - iterator.sheet.firstColumn],
+          sheet5x2DataAoa[position][columns[1] - iterator.sheet.firstColumn],
+          sheet5x2DataAoa[position][columns[2] - iterator.sheet.firstColumn],
+        ]);
+      });
+
+      it('should change position if move argument does not specified', () => {
+        iterator.getRow(position, null);
+        expect(iterator.position).to.deep.equal(position + 1);
+      });
+
+      it('should change position if move argument equals true', () => {
+        iterator.getRow(position, null, true);
+        expect(iterator.position).to.deep.equal(position + 1);
+      });
+
+      it('should not change position if move argument equals false', () => {
+        iterator.getRow(null, null, false);
+        expect(iterator.position).to.deep.equal(position);
+      });
+
+      it('should return null if row index lesser than first row index', () => {
+        const record = iterator.getRow(iterator.sheet.firstRow - 1, null, false);
+        expect(record).to.equal(null);
+      });
+
+      it('should return null if row index greater than last row index', () => {
+        const record = iterator.getRow(iterator.sheet.lastRow + 1, null, false);
+        expect(record).to.equal(null);
+      });
+    });
+
     describe('1x1-offset', () => {
       beforeEach(() => {
         iterator = new Iterator(sheet1x1OffsetObject);
@@ -250,68 +312,6 @@ describe('Iterator', () => {
         const record = iterator.getRow(position, columns);
         expect(record).to.deep.equal([
           sheet2x2OffsetDataAoa[position][columns[0] - iterator.sheet.firstColumn],
-        ]);
-      });
-
-      it('should change position if move argument does not specified', () => {
-        iterator.getRow(position, null);
-        expect(iterator.position).to.deep.equal(position + 1);
-      });
-
-      it('should change position if move argument equals true', () => {
-        iterator.getRow(position, null, true);
-        expect(iterator.position).to.deep.equal(position + 1);
-      });
-
-      it('should not change position if move argument equals false', () => {
-        iterator.getRow(null, null, false);
-        expect(iterator.position).to.deep.equal(position);
-      });
-
-      it('should return null if row index lesser than first row index', () => {
-        const record = iterator.getRow(iterator.sheet.firstRow - 1, null, false);
-        expect(record).to.equal(null);
-      });
-
-      it('should return null if row index greater than last row index', () => {
-        const record = iterator.getRow(iterator.sheet.lastRow + 1, null, false);
-        expect(record).to.equal(null);
-      });
-    });
-
-    describe('5x2', () => {
-      beforeEach(() => {
-        iterator = new Iterator(sheet5x2Object);
-        position = iterator.position;
-      });
-
-      it('should return row with index of current iterator position if index argument does not specified', () => {
-        const record = iterator.getRow();
-        expect(record).to.deep.equal(sheet5x2DataAoa[position]);
-      });
-
-      it('should return row with index of current iterator position if index argument equals null', () => {
-        const record = iterator.getRow(null);
-        expect(record).to.deep.equal(sheet5x2DataAoa[position]);
-      });
-
-      it('should return whole row if columns argument does not specified', () => {
-        const record = iterator.getRow(position);
-        expect(record).to.deep.equal(sheet5x2DataAoa[position]);
-      });
-
-      it('should return whole row if columns argument equals null', () => {
-        const record = iterator.getRow(position, null);
-        expect(record).to.deep.equal(sheet5x2DataAoa[position]);
-      });
-
-      it('should return row with specified only columns', () => {
-        const columns = [1, 2, 4];
-        const record = iterator.getRow(position, columns);
-        expect(record).to.deep.equal([
-          sheet5x2DataAoa[position][columns[0] - iterator.sheet.firstColumn],
-          sheet5x2DataAoa[position][columns[1] - iterator.sheet.firstColumn],
-          sheet5x2DataAoa[position][columns[2] - iterator.sheet.firstColumn],
         ]);
       });
 

ff4f49c17ffb588a4144a6865a134dad22c5d47f
eunikitin <eu.nikitin@gmail.com>
2017-06-04 22:04:23 +0300

    Remove getRow method

--- src/iterator.js
+++ src/iterator.js
@@ -9,28 +9,4 @@ export default class Iterator {
     return columns.sort((a, b) => a - b)
       .filter((value, index, self) => self.indexOf(value) === index);
   }
-
-  getRow(index, columns, move = true) {
-    const row = (index === undefined || index === null) ? this.position : index;
-
-    if (this.sheet.firstRow > row || row > this.sheet.lastRow) return null;
-
-    const self = this;
-
-    const columnList = columns ? Iterator.selectColumns(columns)
-      : Iterator.selectColumns(
-        Array(null, ...{ length: self.sheet.width - 1 }) // eslint-disable-line no-array-constructor
-          .map((arg1, columnIndex) => self.sheet.lastColumn - columnIndex, Number),
-      );
-
-    const record = [];
-
-    columnList.forEach((column) => {
-      record.push(this.sheet.aoa[self.sheet.lastRow - row][self.sheet.lastColumn - column]);
-    });
-
-    if (move) this.position += 1;
-
-    return record;
-  }
 }
